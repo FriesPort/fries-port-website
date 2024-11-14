c5623df624468f3de52811c4a01b05b13dcef614
Vincent Zhong <suppernoname@163.com>
2024-11-14 22:13:42 +0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,7 +20,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/intro">
-            This is FriesPort!
+            Start with FriesPort!
           </Link>
         </div>
       </div>
